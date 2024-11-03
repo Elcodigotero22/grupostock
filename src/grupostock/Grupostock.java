859fda2d1980987cd83db970453cf74fa3dcc1f6
joseh <joseh@GWTN141-10>
2024-11-03 19:53:13 -0400

    Prueba

---
+++ src/grupostock/Grupostock.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupostock;
+
+/**
+ *
+ * @author joseh
+ */
+public class Grupostock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
