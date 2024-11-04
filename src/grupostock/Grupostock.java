0e10d781fdbe7938c7aab356692bddfe1988b98f
joseh <joseh@GWTN141-10>
2024-11-03 20:30:45 -0400

    a;adi un 4 hola mundo

--- src/grupostock/Grupostock.java
+++ src/grupostock/Grupostock.java
@@ -17,6 +17,7 @@ public class Grupostock {
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
     
 }
